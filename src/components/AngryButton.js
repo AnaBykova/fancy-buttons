f09da6e9ebc48c1bbbb18656ef00207b55b2710a
AnaBykova <anabykovaca@gmail.com>
2023-10-12 19:05:02 -0600

    modify State

--- src/components/AngryButton.js
+++ src/components/AngryButton.js
@@ -1,9 +1,17 @@
 import { useState } from "react";
 
-function AngryButton(){
+function AngryButton(props){
   const [anger, setAnger] = useState(0);
-  return (
-    <button style={{backgroundColor: `rgba(255,0,0,${anger})` }} className="AngryButton" >
+  const handleClick = (amount) => {
+    if (anger < 1) {
+      setAnger(anger + amount);
+    } else {
+      setAnger(0);
+    }
+  }
+
+  return (    
+    <button style={{backgroundColor: `rgba(255,0,0,${anger})` }} onClick={() => handleClick(0.1)} className="AngryButton" >
       {anger < 1 && <span>Don't click me too much! </span>}
       {anger > 1 && <span>Rawr!</span>}
     </button>
